8d352ed9ee48370ddfc31fe748aa7410a87897ee
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2021-09-30 21:17:06 -0300

    start methods

---
+++ dijkstra.py
@@ -0,0 +1,25 @@
+class Heap(object):
+	pass
+
+def get_airports(path='airports.dat'):
+	pass
+
+def get_flights(path='flights.dat'):
+	pass
+
+class Graph(object):
+	pass
+
+    @classmethod
+    def load(cls):
+	pass
+
+    @staticmethod
+    def get_price(origin, destination, cents_per_km=0.1):
+	pass
+
+    def dijkstra(self, origin, destination):
+	pass
+
+if __name__ == "__main__":
+
